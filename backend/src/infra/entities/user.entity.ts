809849ae53d0c4729feab06f44a12bc996808577
Ian García <ian.garcia@nibbio.co>
2023-03-09 13:23:15 -0600

    [BACKEND] Added Infra User entity.

---
+++ backend/src/infra/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column({ default: "" })
+  firstName!: string;
+
+  @Column({ default: "" })
+  lastName!: string;
+
+  constructor(user: Partial<User>) {
+    Object.assign(this, user);
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
