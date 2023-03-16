52c5c4d325418e82a8c9b88f15745d8845ffa402
Ian García <ian.garcia@nibbio.co>
2023-03-16 12:42:32 -0600

    [INFRA] Implemented custom API Error.

---
+++ backend/src/utils/APIError.ts
@@ -0,0 +1,20 @@
+// (c) Nibbio 2023, rights reserved.
+
+class APIError extends Error {
+  statusCode: number;
+
+  status: string;
+
+  isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default APIError;
