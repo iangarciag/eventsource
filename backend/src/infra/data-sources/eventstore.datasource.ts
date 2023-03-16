f8c77d1d365b0685e926ea807fb44da448fb97ad
Ian García <ian.garcia@nibbio.co>
2023-03-16 12:41:25 -0600

    [BACKEND] Improved TypeORM connection.

--- backend/src/infra/data-sources/eventstore.datasource.ts
+++ backend/src/infra/data-sources/eventstore.datasource.ts
@@ -1,3 +1,20 @@
 // (c) Nibbio 2023, rights reserved.
 
-// WIP
+import { DataSource } from "typeorm";
+import { Application } from "../entities/application.entity";
+import { Role } from "../entities/role.entity";
+import { User } from "../entities/user.entity";
+
+export const dataSource: DataSource = new DataSource({
+  type: "postgres",
+  host: "postgres",
+  port: 5432,
+  username: "pguser",
+  password: "pgpassword",
+  database: "pgdb",
+  entities: [Application, Role, User],
+
+  synchronize: true,
+  logging: true,
+  ssl: false,
+});
