f10e4353f314b0c0afb0e02d6e7661fa6b97df3d
Ian García <ian.garcia@nibbio.co>
2023-03-22 14:30:01 -0600

    [BACKEND] Added missing TypeORM Entities.

--- backend/src/infra/data-sources/eventstore.datasource.ts
+++ backend/src/infra/data-sources/eventstore.datasource.ts
@@ -4,6 +4,8 @@ import { DataSource } from "typeorm";
 import { Application } from "../entities/application.entity";
 import { Role } from "../entities/role.entity";
 import { User } from "../entities/user.entity";
+import { Permission } from "../entities/permission.entity";
+import { Resource } from "../entities/resource.entity";
 
 export const dataSource: DataSource = new DataSource({
   type: "postgres",
@@ -12,7 +14,7 @@ export const dataSource: DataSource = new DataSource({
   username: "pguser",
   password: "pgpassword",
   database: "pgdb",
-  entities: [Application, Role, User],
+  entities: [Application, Role, User, Resource, Permission],
 
   synchronize: true,
   logging: true,
